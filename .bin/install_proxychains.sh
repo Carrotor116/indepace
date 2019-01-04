7b94d568dad01e04241a7a13031c723aa10f18f3
Nonu <1162365377@qq.com>
2019-01-04 21:54:07 +1100

    modify string

--- .bin/install_proxychains.sh
+++ .bin/install_proxychains.sh
@@ -9,5 +9,5 @@ fi
 
 git clone https://github.com/rofl0r/proxychains-ng.git ~/.opt/proxychains-ng 
 cd ~/.opt/proxychains-ng && ./configure && make
-echo "alias proxychains4='/home/nls1/nonu/.opt/proxychains-ng/proxychains4 -f /home/nls1/nonu/.opt/proxychains-ng/src/proxychains.conf'" >> ~/.alias
+echo "alias proxychains4='~/.opt/proxychains-ng/proxychains4 -f ~/.opt/proxychains-ng/src/proxychains.conf'" >> ~/.alias
 echo -e "\ninstall succeed\nyou need to point out proxy server in ~/.opt/proxychains-ng/src/proxychains.conf to make sure proxychains4 workround\n"
